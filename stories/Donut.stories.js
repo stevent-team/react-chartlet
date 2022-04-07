e74da5287e033a7887e010fe70d3d3ec7d438180
Ewan Breakey <ebreakey@gmail.com>
2022-04-07 22:20:11 +1000

    Update stories

--- stories/Donut.stories.js
+++ stories/Donut.stories.js
@@ -13,6 +13,34 @@ Unstyled.args = {
   height: 300,
 }
 
+export const PieChart = Template.bind({})
+PieChart.args = {
+  data: generateData(),
+  height: 300,
+  hole: 0,
+}
+
+export const FiftyFiftyDonut = Template.bind({})
+FiftyFiftyDonut.args = {
+  data: [1, 1],
+  height: 300,
+  hole: .5,
+}
+
+export const FiftyFiftyOffsetDonut = Template.bind({})
+FiftyFiftyOffsetDonut.args = {
+  data: [1, 1],
+  height: 300,
+  hole: .5,
+  offset: .25,
+}
+
+export const OneCategory = Template.bind({})
+OneCategory.args = {
+  data: [1],
+  height: 300,
+}
+
 export const ManyCategories = Template.bind({})
 ManyCategories.args = {
   data: generateLotsOfData(),
@@ -23,9 +51,9 @@ export const Styled = Template.bind({})
 Styled.args = {
   data: generateData(),
   width: 400,
-  height: 100,
-  style: { border: '4px solid rebeccapurple', background: 'mistyrose' },
-  lineStyle: {
+  height: 400,
+  style: { border: '4px solid rebeccapurple', background: 'mistyrose'},
+  segmentStyle: {
     stroke: 'rebeccapurple',
     strokeLinecap: 'round',
     strokeLinejoin: 'round',
@@ -34,8 +62,23 @@ Styled.args = {
   margin: 10,
 }
 
-export const FlatLine = Template.bind({})
-FlatLine.args = {
-  data: [1],
-  height: 300
+export const SignatureSegment = args => <>
+  <svg width={0} height={0}>
+    <defs>
+      <linearGradient id="myGradient">
+        <stop offset="50%"   stop-color="white" />
+        <stop offset="100%" stop-color="transparent" />
+      </linearGradient>
+    </defs>
+  </svg>
+  <Donut {...args} />
+</>
+SignatureSegment.args = {
+  data: generateData(),
+  width: 400,
+  height: 400,
+  segmentStyles: [null, {
+    fill: 'url(#myGradient)',
+  }],
+  margin: 10,
 }
