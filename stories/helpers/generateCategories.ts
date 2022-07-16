d32fbb5684ed7ede7a46ccecc94ce7286adb54e5
Ewan Breakey <ebreakey@gmail.com>
2022-07-17 01:06:20 +1000

    Use letters for sample categories in stories

--- stories/helpers/generateCategories.ts
+++ stories/helpers/generateCategories.ts
@@ -1,5 +1,7 @@
+const LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVXWYZ'.split('')
+
 export const generateCategories = (length = 3) => Object.fromEntries(
     Array
       .from({length}, () => Math.floor(Math.random() * 40))
-      .map((v, i) => [String(i), v])
+      .map((v, i) => [LETTERS[i % LETTERS.length], v])
   )
